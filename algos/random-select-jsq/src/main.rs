57f27d9b1d057e5b91970e7aec6ed2dc577234e0
Matthias Beyer <mail@beyermatthias.de>
2016-12-17 14:59:12 +0100

    Add random-select-jsq impl

---
+++ algos/random-select-jsq/src/main.rs
@@ -0,0 +1,25 @@
+extern crate csv;
+extern crate random;
+extern crate libcommon;
+
+use std::env;
+use libcommon::*;
+use random::Source;
+
+fn main() {
+    let (nodes, tasks)   = load(env::args().collect());
+    let mut ass : Vec<_> = nodes.into_iter().map(Assignment::new).collect();
+    let nass = ass.len();
+
+    let mut source = random::default().seed([42, 1337]);
+
+    for task in tasks.into_iter() {
+        let i : usize = source.read::<usize>() % nass;
+        let j : usize = source.read::<usize>() % nass;
+
+        let x = if ass[i].len() <= ass[j].len() { i } else { j };
+        ass[x].push(task);
+    }
+
+    write_out(ass);
+}
